fd0058f69ff93b0e4e05fa71f6bcd2e3d6a956a3
Justin Shetty <jas0504@gmail.com>
2024-10-25 01:20:22 +0900

    extend test_scatter

--- tests/test_scatter.cpp
+++ tests/test_scatter.cpp
@@ -7,9 +7,37 @@ int main()
 {
   const int VERBOSITY = 0;
 
+  // two agents, one goal each
+  {
+    const auto map_filename = "../assets/empty-8-8.map";
+    // upper left corner
+    //  0  1  2
+    //  8  9 10
+    // 16 17 18
+
+    const std::vector<int> starts = {8, 17};
+    std::vector<std::vector<int>> goal_sequences = {{18}, {1}};
+    const auto ins = Instance(map_filename, starts, goal_sequences);
+    assert(ins.is_valid(VERBOSITY));
+
+    DistTableMultiGoal D = DistTableMultiGoal(&ins);
+    const auto sc_margin = 0;
+    Scatter sc = Scatter(&ins, &D, nullptr, 0, VERBOSITY, sc_margin);
+    sc.construct();
+
+    std::vector<std::vector<int>> expected = {{8, 16, 17, 18}, {17, 9, 1}};
+    for (int i = 0; i < ins.N; ++i) {
+      for (int j = 0; j < sc.paths[i].size(); ++j) {
+        assert(sc.paths[i][j]->id == expected[i][j]);
+      }
+    }
+  }
+
   // single agent, multiple goals
   {
     const auto map_filename = "../tests/assets/2x2.map";
+    // 0 1
+    // 2 3
 
     const std::vector<int> starts = {0};
     std::vector<std::vector<int>> goal_sequences = {{3, 0}};
@@ -21,13 +49,7 @@ int main()
     Scatter sc = Scatter(&ins, &D, nullptr, 0, VERBOSITY, sc_margin);
     sc.construct();
 
-    for (int i = 0; i < ins.N; ++i) {
-      std::cout << "sc.paths[" << i << "]: ";
-      for (const auto& v : sc.paths[i]) {
-        std::cout << v->id << " ";
-      }
-      std::cout << std::endl;
-    }
+    assert(sc.paths[0].size() == 5);
   }
 
   return 0;
